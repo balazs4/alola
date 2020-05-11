5822a4ec5ee2698f1dfeeccbc1fb95ce8ade87c9
balazs4 <balazs4web@gmail.com>
2020-05-12 00:04:18 +0200

    chore: remove tap-set-exit

--- index.test.js
+++ index.test.js
@@ -1,7 +1,12 @@
-const test = require('baretest')('alola');
+const [, , ref = '...'] = process.argv;
+const test = require('baretest')(`${Date.now()} alola #${ref}`);
 
 require('./finalize.test')(test);
 require('./parse.test')(test);
 require('./split.test')(test);
 
-test.run();
+test.run().then((allpassed) => {
+  if (allpassed === false) {
+    process.exit(-1);
+  }
+});
