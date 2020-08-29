12f55c6657672ca51191a80f12c5bcbcf69c8923
balazs4 <balazs4web@gmail.com>
2020-08-29 16:40:27 +0200

    test: create real e2e test

--- e2e.sh
+++ e2e.sh
@@ -2,29 +2,23 @@
 
 # preconditions
 #
+# curl
+# netcat
+# npm link or npm i -g alola
 # npm i -g fx@latest
 # patch ~/.fxrc 
 
-echo -e '
-{
-  "redirects": [],
-  "protocol": "HTTP/2",
-  "status": 200,
-  "statusText": "",
-  "headers": {
-    "content-type": "application/json; charset=utf-8",
-    "x-powered-by": "Express"
-  },
-  "body": {
-    "id": 42,
-    "foo": "bar",
-    "nested": {
-      "id": 42,
-      "foo": "bar"
-    }
-  }
-}
-' | fx 'alola()' \
+PORT=9000
+
+(echo -e 'HTTP/2 200
+Content-Type: application/json; charset=utf-8
+X-Powered-By: Express
+
+{ "id": 42, "foo": "bar", "nested": { "id": 42, "foo": "bar" }}' | nc -c -l -p ${PORT} &)
+
+curl -Lis http://localhost:${PORT} \
+ | alola \
+ | fx 'alola()' \
   'expect("status", 200)' \
   'expect("status", /^2\d{2}$/)' \
   'unexpect("status", 404)' \
@@ -36,3 +30,24 @@ echo -e '
   'expect("body.nested.foo", "bar")' \
   'expect("body.nested", { "id": 42, "foo": "bar" })' \
   'custom("I do it better", (json, assert) => assert.ok(true))'
+
+  # echo -e '
+  # {
+  #   "redirects": [],
+  #   "protocol": "HTTP/2",
+  #   "status": 200,
+  #   "statusText": "",
+  #   "headers": {
+  #     "content-type": "application/json; charset=utf-8",
+  #     "x-powered-by": "Express"
+  #   },
+  #   "body": {
+  #     "id": 42,
+  #     "foo": "bar",
+  #     "nested": {
+  #       "id": 42,
+  #       "foo": "bar"
+  #     }
+  #   }
+  # }
+  # '
