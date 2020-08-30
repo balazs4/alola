45d8669b04e29a902e19e574dbd35465a42ce2d1
balazs4 <balazs4web@gmail.com>
2020-08-30 22:48:03 +0200

    ci: add netcat function

--- e2e.sh
+++ e2e.sh
@@ -10,44 +10,31 @@
 
 PORT=9000
 
+function netcat(){
+  GNU=`nc --version > /dev/null; echo $?`
+  if [ $GNU -eq 0 ]
+    then nc -c -l -p $1 > /dev/null
+    else nc -N -l $1 > /dev/null 2>&1
+  fi
+}
+
 (echo -e 'HTTP/2 200
 Content-Type: application/json; charset=utf-8
 X-Powered-By: Express
 
-{ "id": 42, "foo": "bar", "nested": { "id": 42, "foo": "bar" }}' | nc -l -p ${PORT} &)
+{ "id": 42, "foo": "bar", "nested": { "id": 42, "foo": "bar" }}' | netcat ${PORT} &)
 
 curl -Lis http://localhost:${PORT} \
  | alola \
  | fx 'alola()' \
-  'expect("status", 200)' \
-  'expect("status", /^2\d{2}$/)' \
-  'unexpect("status", 404)' \
-  'unexpect("status", /^4\d{2}$/)' \
-  'expect("headers.x-powered-by", "Express")' \
-  'expect("body.id", 42)' \
-  'expect("body.id", /^\d{2}$/)' \
-  'expect("body.foo", "bar")' \
-  'expect("body.nested.foo", "bar")' \
-  'expect("body.nested", { "id": 42, "foo": "bar" })' \
-  'custom("I do it better", (json, assert) => assert.ok(true))'
-
-  # echo -e '
-  # {
-  #   "redirects": [],
-  #   "protocol": "HTTP/2",
-  #   "status": 200,
-  #   "statusText": "",
-  #   "headers": {
-  #     "content-type": "application/json; charset=utf-8",
-  #     "x-powered-by": "Express"
-  #   },
-  #   "body": {
-  #     "id": 42,
-  #     "foo": "bar",
-  #     "nested": {
-  #       "id": 42,
-  #       "foo": "bar"
-  #     }
-  #   }
-  # }
-  # '
+   'expect("status", 200)' \
+   'expect("status", /^2\d{2}$/)' \
+   'unexpect("status", 404)' \
+   'unexpect("status", /^4\d{2}$/)' \
+   'expect("headers.x-powered-by", "Express")' \
+   'expect("body.id", 42)' \
+   'expect("body.id", /^\d{2}$/)' \
+   'expect("body.foo", "bar")' \
+   'expect("body.nested.foo", "bar")' \
+   'expect("body.nested", { "id": 42, "foo": "bar" })' \
+   'custom("I do it better", (json, assert) => assert.ok(true))'
