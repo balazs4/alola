761e0f4b196e407178f23e5feba4df46c716ce09
balazs4 <balazs4web@gmail.com>
2020-08-30 23:17:13 +0200

    ci: :fingers_crossed:

--- e2e.sh
+++ e2e.sh
@@ -11,10 +11,9 @@
 PORT=9000
 
 function netcat(){
-  GNU=`nc --version > /dev/null; echo $?`
-  if [ $GNU -eq 0 ]
-    then nc -c -l -p $1 > /dev/null
-    else nc -N -l $1 > /dev/null 2>&1
+  if [ $CI = 'true' ]
+    then nc -N -l $1 > /dev/null; sleep 4s;  # sleep because github actions is not blazing-fast
+    else nc -c -l -p $1 > /dev/null
   fi
 }
 
@@ -24,20 +23,17 @@ X-Powered-By: Express
 
 { "id": 42, "foo": "bar", "nested": { "id": 42, "foo": "bar" }}' | netcat ${PORT} &)
 
-[ $CI ] && sleep 4s
-
-curl -Lis http://localhost:${PORT} 
-# curl -Lis http://localhost:${PORT} \
-#  | alola \
-#  | fx 'alola()' \
-#    'expect("status", 200)' \
-#    'expect("status", /^2\d{2}$/)' \
-#    'unexpect("status", 404)' \
-#    'unexpect("status", /^4\d{2}$/)' \
-#    'expect("headers.x-powered-by", "Express")' \
-#    'expect("body.id", 42)' \
-#    'expect("body.id", /^\d{2}$/)' \
-#    'expect("body.foo", "bar")' \
-#    'expect("body.nested.foo", "bar")' \
-#    'expect("body.nested", { "id": 42, "foo": "bar" })' \
-#    'custom("I do it better", (json, assert) => assert.ok(true))'
+curl -Lis http://localhost:${PORT} \
+ | alola \
+ | fx 'alola()' \
+   'expect("status", 200)' \
+   'expect("status", /^2\d{2}$/)' \
+   'unexpect("status", 404)' \
+   'unexpect("status", /^4\d{2}$/)' \
+   'expect("headers.x-powered-by", "Express")' \
+   'expect("body.id", 42)' \
+   'expect("body.id", /^\d{2}$/)' \
+   'expect("body.foo", "bar")' \
+   'expect("body.nested.foo", "bar")' \
+   'expect("body.nested", { "id": 42, "foo": "bar" })' \
+   'custom("I do it better", (json, assert) => assert.ok(true))'
