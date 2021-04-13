51ef4a1367e2bd69ebc74be6c5d714bb1e9cda4e
Balazs Varga <balazs4@users.noreply.github.com>
2021-04-13 23:55:08 +0200

    feat: hello bft (#12)
    solong baretest, hey bft
    bump deps

--- split.test.js
+++ split.test.js
@@ -1,113 +1,111 @@
-const assert = require('assert');
+const assert = require('assert').strict;
 const split = require('./split');
 
-module.exports = (test) => {
-  test('split > 1 response without body (aka headers only)', () => {
-    const input = [
+test('split > 1 response without body (aka headers only)', () => {
+  const input = [
+    'HTTP/2 200 OK',
+    'Server: dummy',
+    'Content-Type: application/json',
+    'x-fake-response: 42',
+    '',
+  ];
+
+  const expected = [
+    [
       'HTTP/2 200 OK',
       'Server: dummy',
       'Content-Type: application/json',
       'x-fake-response: 42',
-      '',
-    ];
+    ],
+  ];
 
-    const expected = [
-      [
-        'HTTP/2 200 OK',
-        'Server: dummy',
-        'Content-Type: application/json',
-        'x-fake-response: 42',
-      ],
-    ];
+  const actual = split(input);
+  assert.deepEqual(actual, expected);
+});
 
-    const actual = split(input);
-    assert.deepEqual(actual, expected);
-  });
+test('split > 1 response with body', () => {
+  const input = [
+    'HTTP/2 200 OK',
+    'Server: dummy',
+    'Content-Type: application/json',
+    'x-fake-response: 42',
+    '',
+    '{"foo": "bar"}',
+  ];
 
-  test('split > 1 response with body', () => {
-    const input = [
-      'HTTP/2 200 OK',
-      'Server: dummy',
-      'Content-Type: application/json',
-      'x-fake-response: 42',
-      '',
-      '{"foo": "bar"}',
-    ];
-
-    const expected = [
-      [
-        'HTTP/2 200 OK',
-        'Server: dummy',
-        'Content-Type: application/json',
-        'x-fake-response: 42',
-        '',
-        '{"foo": "bar"}',
-      ],
-    ];
-
-    const actual = split(input);
-    assert.deepEqual(actual, expected);
-  });
-
-  test('split > 2 responses with body', () => {
-    const input = [
-      'HTTP/1.1 301',
-      'Location: http://newurl.com/',
-      '',
+  const expected = [
+    [
       'HTTP/2 200 OK',
       'Server: dummy',
       'Content-Type: application/json',
       'x-fake-response: 42',
       '',
       '{"foo": "bar"}',
-    ];
+    ],
+  ];
 
-    const expected = [
-      ['HTTP/1.1 301', 'Location: http://newurl.com/'],
-      [
-        'HTTP/2 200 OK',
-        'Server: dummy',
-        'Content-Type: application/json',
-        'x-fake-response: 42',
-        '',
-        '{"foo": "bar"}',
-      ],
-    ];
+  const actual = split(input);
+  assert.deepEqual(actual, expected);
+});
 
-    const actual = split(input);
-    assert.deepEqual(actual, expected);
-  });
+test('split > 2 responses with body', () => {
+  const input = [
+    'HTTP/1.1 301',
+    'Location: http://newurl.com/',
+    '',
+    'HTTP/2 200 OK',
+    'Server: dummy',
+    'Content-Type: application/json',
+    'x-fake-response: 42',
+    '',
+    '{"foo": "bar"}',
+  ];
 
-  test('split > 3 responses with body', () => {
-    const input = [
-      'HTTP/1.1 301',
-      'Location: http://newurl.com/',
-      '',
-      'HTTP/2 301',
-      'Location: https://newurl.com/',
-      '',
+  const expected = [
+    ['HTTP/1.1 301', 'Location: http://newurl.com/'],
+    [
       'HTTP/2 200 OK',
       'Server: dummy',
       'Content-Type: application/json',
       'x-fake-response: 42',
       '',
       '{"foo": "bar"}',
-    ];
+    ],
+  ];
 
-    const expected = [
-      ['HTTP/1.1 301', 'Location: http://newurl.com/'],
-      ['HTTP/2 301', 'Location: https://newurl.com/'],
-      [
-        'HTTP/2 200 OK',
-        'Server: dummy',
-        'Content-Type: application/json',
-        'x-fake-response: 42',
-        '',
-        '{"foo": "bar"}',
-      ],
-    ];
+  const actual = split(input);
+  assert.deepEqual(actual, expected);
+});
 
-    const actual = split(input);
-    assert.deepEqual(actual, expected);
-  });
-};
+test('split > 3 responses with body', () => {
+  const input = [
+    'HTTP/1.1 301',
+    'Location: http://newurl.com/',
+    '',
+    'HTTP/2 301',
+    'Location: https://newurl.com/',
+    '',
+    'HTTP/2 200 OK',
+    'Server: dummy',
+    'Content-Type: application/json',
+    'x-fake-response: 42',
+    '',
+    '{"foo": "bar"}',
+  ];
+
+  const expected = [
+    ['HTTP/1.1 301', 'Location: http://newurl.com/'],
+    ['HTTP/2 301', 'Location: https://newurl.com/'],
+    [
+      'HTTP/2 200 OK',
+      'Server: dummy',
+      'Content-Type: application/json',
+      'x-fake-response: 42',
+      '',
+      '{"foo": "bar"}',
+    ],
+  ];
+
+  const actual = split(input);
+  assert.deepEqual(actual, expected);
+});
