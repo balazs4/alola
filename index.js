4f02953c60896f3da2a3240daab5f104ddf334a6
balazs4 <balazs4@users.noreply.github.com>
2021-12-04 12:41:45 +0100

    chore: refactor index

--- index.js
+++ index.js
@@ -6,18 +6,13 @@ const parse = require('./parse');
 const finalize = require('./finalize');
 const check = require('./check');
 
-const write = (json) => {
-  if (process.env.ALOLA_REPORT_ONLY){
-    return json;
-  }
-  console.log(JSON.stringify(json, null, 2));
-  return json; 
-};
-
 const [, , ...assertions] = process.argv;
-read()
-  .then((lines) => split(lines))
-  .then((blocks) => blocks.map((block) => parse(block)))
-  .then((parsedblocks) => finalize(parsedblocks))
-  .then((json) => write(json))
-  .then((json) => check(json, assertions));
+
+read().then((lines) => {
+  const blocks = split(lines).map((block) => parse(block));
+  const json = finalize(blocks);
+  if (process.env.ALOLA_REPORT_ONLY === undefined) {
+    console.log(JSON.stringify(json, null, 2));
+  }
+  check(json, assertions);
+});
