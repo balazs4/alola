5300a78116e30960300a6e75c071c4050345fda1
balazs4 <balazs4@users.noreply.github.com>
2022-05-15 14:45:51 +0200

    feat: support undefined as expected value
    fix: use falsy value as is (e.g. false)

--- e2e.sh
+++ e2e.sh
@@ -8,7 +8,7 @@ function fakecurl(){
 Content-Type: application/json; charset=utf-8
 X-Powered-By: Express
 
-{ "id": 42, "foo": "bar", "nested": { "id": 42, "foo": "bar" }, "tags": ["awesome", "cool"], "whatif": "should be"}'
+{ "id": 42, "foo": "bar", "nested": { "id": 42, "foo": "bar" }, "tags": ["awesome", "cool"], "whatif": "should be", "yes": true, "elements": [{ "public": false }, { "public": true }]}'
 }
 
 fakecurl -Lis http://localhost:4242 \
@@ -24,9 +24,14 @@ fakecurl -Lis http://localhost:4242 \
   'body.foo should be bar' \
   'body.tags.0 should be awesome' \
   'body.tags.length should be 2' \
-  'body.tags.4 should not be defined' \
+  'body.tags.4 should be undefined' \
   'body.nested.foo should be bar' \
   'body.nested should be { "id": 42, "foo": "bar" }' \
   'body.whatif should be should be' \
+  'body.error should be undefined' \
+  'body.yes should be true' \
+  'body.elements.0.public should be false' \
+  'body.elements.1.public should be true' \
+  'body.elements.2.public should be undefined' \
   'alola is awesome' \
   'alola is fantastic'
